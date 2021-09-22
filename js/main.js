c97ac5661c824a0dccf5cc4bc7e68590379141b5
Kenneth Wang <yunfeiw@me.com>
2021-09-22 17:10:31 +0000

    Completed the 2nd feature

--- js/main.js
+++ js/main.js
@@ -1,28 +1,54 @@
 var $breed = document.querySelector('.breed');
 var $random = document.querySelector('.random');
 var $form = document.querySelector('form');
+var $opButton = document.querySelectorAll('.opButton');
+var $buttonLeft = document.querySelector('.buttonLeft');
+var $wholePage = document.querySelector('.wholePage');
+var $startButton = document.querySelector('.startButton');
+var $appCover = document.querySelector('.appCover');
+var picViewNumber = 1;
 // var $picView = document.querySelector('.picView');
+
+$startButton.addEventListener('click', function () {
+  $wholePage.className = 'wholePage';
+  $appCover.className = 'appCover hidden';
+
+});
+
+$buttonLeft.addEventListener('click', function () {
+  if (event.target.matches('.opButton') === false) {
+    return;
+  }
+  for (var i = 0; i < $opButton.length; i++) {
+    if ($opButton[i] === event.target) {
+      $opButton[i].className = 'opButton opClicked';
+      picViewNumber = parseInt($opButton[i].getAttribute('data-number'));
+
+    } else {
+      $opButton[i].className = 'opButton';
+    }
+  }
+});
 
 $breed.addEventListener('click', function () {
   $form.className = 'column-half';
   $breed.className = 'breed picButton clicked';
 });
 
-/* <div class="row allEntries" >
-    <div class=" onePic column-two-third">
+/*
+    <div class=" onePic column-two-third" data-pic="">
       <div class="picHolder"><img class="picView" src="https://images.dog.ceo/breeds/hound-walker/n02089867_3103.jpg"></div>
       <div class="saveButtonHolder">
-       <button class="picButton" type="button">SAVE</button>
+       <button class="picButton" type="button" data-pic="">SAVE</button>
       </div>
     </div>
-  </div> */
+   */
 
 function renderPic(data) {
-  var $allEntries = document.createElement('div');
-  $allEntries.setAttribute('class', 'row allEntries');
 
   var $onePic = document.createElement('div');
   $onePic.setAttribute('class', 'onePic column-two-third');
+  $onePic.setAttribute('data-pic', data.message);
 
   var $picHolder = document.createElement('div');
   $picHolder.setAttribute('class', 'picHolder');
@@ -38,32 +64,140 @@ function renderPic(data) {
   $saveButton.setAttribute('class', 'picButton');
   $saveButton.setAttribute('type', 'button');
   $saveButton.textContent = 'SAVE';
+  $saveButton.setAttribute('data-pic', data.message);
 
-  $allEntries.appendChild($onePic);
+  // $allEntries.appendChild($onePic);
   $onePic.appendChild($picHolder);
   $onePic.appendChild($buttonHolder);
   $picHolder.appendChild($picView);
   $buttonHolder.appendChild($saveButton);
 
-  return $allEntries;
+  return $onePic;
 
 }
 
+// for  2 pic showing
+/*
+    <div class=" morePic column-half" data-pic="">
+      <div class="picHolder"><img class="picView-two" src="https://images.dog.ceo/breeds/hound-walker/n02089867_3103.jpg"></div>
+      <div class="saveButtonHolder">
+       <button class="picButton" type="button" data-pic="">SAVE</button>
+      </div>
+    </div>
+   */
+
+function renderMorePic(data) {
+
+  var $onePic = document.createElement('div');
+  $onePic.setAttribute('class', 'morePic column-half');
+  $onePic.setAttribute('data-pic', data);
+
+  var $picHolder = document.createElement('div');
+  $picHolder.setAttribute('class', 'picHolder');
+
+  var $picView = document.createElement('img');
+  $picView.setAttribute('class', 'picView-two');
+  $picView.setAttribute('src', data);
+
+  var $buttonHolder = document.createElement('div');
+  $buttonHolder.setAttribute('class', 'saveButtonHolder');
+
+  var $saveButton = document.createElement('button');
+  $saveButton.setAttribute('class', 'picButton');
+  $saveButton.setAttribute('type', 'button');
+  $saveButton.textContent = 'SAVE';
+  $saveButton.setAttribute('data-pic', data);
+
+  $onePic.appendChild($picHolder);
+  $onePic.appendChild($buttonHolder);
+  $picHolder.appendChild($picView);
+  $buttonHolder.appendChild($saveButton);
+
+  return $onePic;
+
+}
+// for  3 pic showing
+/*
+    <div class=" morePic column-one-forth" data-pic="">
+      <div class="picHolder"><img class="picView-three" src="https://images.dog.ceo/breeds/hound-walker/n02089867_3103.jpg"></div>
+      <div class="saveButtonHolder">
+       <button class="picButton" type="button" data-pic="">SAVE</button>
+      </div>
+    </div>
+   */
+
+function renderThreePic(data) {
+
+  var $onePic = document.createElement('div');
+  $onePic.setAttribute('class', 'morePic column-one-forth');
+  $onePic.setAttribute('data-pic', data);
+
+  var $picHolder = document.createElement('div');
+  $picHolder.setAttribute('class', 'picHolder');
+
+  var $picView = document.createElement('img');
+  $picView.setAttribute('class', 'picView-three');
+  $picView.setAttribute('src', data);
+
+  var $buttonHolder = document.createElement('div');
+  $buttonHolder.setAttribute('class', 'saveButtonHolder');
+
+  var $saveButton = document.createElement('button');
+  $saveButton.setAttribute('class', 'picButton');
+  $saveButton.setAttribute('type', 'button');
+  $saveButton.textContent = 'SAVE';
+  $saveButton.setAttribute('data-pic', data);
+
+  $onePic.appendChild($picHolder);
+  $onePic.appendChild($buttonHolder);
+  $picHolder.appendChild($picView);
+  $buttonHolder.appendChild($saveButton);
+
+  return $onePic;
+
+}
 var $pictureList = document.querySelector('#picture-list');
+
+// here starting the searching code
 
 function randomPicSearch() {
 
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://dog.ceo/api/breeds/image/random');
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
+  if (picViewNumber === 1) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://dog.ceo/api/breeds/image/random');
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
 
-    var newPic = renderPic(xhr.response);
-    $pictureList.appendChild(newPic);
+      var newPic = renderPic(xhr.response);
+      $pictureList.appendChild(newPic);
 
-  });
-  xhr.send();
+    });
+    xhr.send();
+  } else if (picViewNumber === 2) {
+    var xhrtwo = new XMLHttpRequest();
+    xhrtwo.open('GET', 'https://dog.ceo/api/breeds/image/random/2');
+    xhrtwo.responseType = 'json';
+    xhrtwo.addEventListener('load', function () {
 
+      for (var a = 0; a < xhrtwo.response.message.length; a++) {
+        var newPicTwo = renderMorePic(xhrtwo.response.message[a]);
+        $pictureList.appendChild(newPicTwo);
+      }
+    });
+    xhrtwo.send();
+  } else if (picViewNumber === 3) {
+    var xhrthree = new XMLHttpRequest();
+    xhrthree.open('GET', 'https://dog.ceo/api/breeds/image/random/3');
+    xhrthree.responseType = 'json';
+    xhrthree.addEventListener('load', function () {
+
+      for (var b = 0; b < xhrthree.response.message.length; b++) {
+        var newPicThree = renderThreePic(xhrthree.response.message[b]);
+        $pictureList.appendChild(newPicThree);
+      }
+    });
+    xhrthree.send();
+  }
 }
 
 $random.addEventListener('click', function () {
@@ -77,17 +211,43 @@ function breedSearch(name) {
   for (var i = 0; i < name.length; i++) {
     breed += name.charAt(i).toLowerCase();
   }
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://dog.ceo/api/breed/' + breed + '/images/random');
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
 
-    var newPic = renderPic(xhr.response);
-    $pictureList.appendChild(newPic);
+  if (picViewNumber === 1) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://dog.ceo/api/breed/' + breed + '/images/random');
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
 
-  });
-  xhr.send();
+      var newPic = renderPic(xhr.response);
+      $pictureList.appendChild(newPic);
 
+    });
+    xhr.send();
+  } else if (picViewNumber === 2) {
+    var xhrtwo = new XMLHttpRequest();
+    xhrtwo.open('GET', 'https://dog.ceo/api/breed/' + breed + '/images/random/2');
+    xhrtwo.responseType = 'json';
+    xhrtwo.addEventListener('load', function () {
+      for (var a = 0; a < xhrtwo.response.message.length; a++) {
+        var newPicTwo = renderMorePic(xhrtwo.response.message[a]);
+        $pictureList.appendChild(newPicTwo);
+      }
+
+    });
+    xhrtwo.send();
+  } else if (picViewNumber === 3) {
+    var xhrthree = new XMLHttpRequest();
+    xhrthree.open('GET', 'https://dog.ceo/api/breed/' + breed + '/images/random/3');
+    xhrthree.responseType = 'json';
+    xhrthree.addEventListener('load', function () {
+      for (var b = 0; b < xhrthree.response.message.length; b++) {
+        var newPicThree = renderThreePic(xhrthree.response.message[b]);
+        $pictureList.appendChild(newPicThree);
+      }
+
+    });
+    xhrthree.send();
+  }
 }
 
 $form.addEventListener('submit', function () {
